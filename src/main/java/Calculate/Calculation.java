05daa638eff233a586360360f42c534ffc921895
kimjongwoo <pocky99312@gmail.com>
2024-06-05 20:20:01 +0900

    1. step1 :  계산기 기능 구현 (사칙연산) 2. step2 : 계산기 기능 구현 토대로 테스트 코드 작성

---
+++ src/main/java/Calculate/Calculation.java
@@ -0,0 +1,22 @@
+package Calculate;
+
+public class Calculation {
+	int num1;
+	int num2;
+
+	public int plusCalculate() {
+		return num1 + num2;
+	}
+
+	public int minusCalculate() {
+		return num1 - num2;
+	}
+
+	public int divCalculate() {
+		return num1 / num2;
+	}
+
+	public int multiCalculate() {
+		return num1 * num2;
+	}
+}
